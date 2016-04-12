fba562f377b0e99fdf87f366916b69e0a7e87390
concreteface <concreteface@users.noreply.github.com>
2016-04-11 20:50:26 -0400

    made filenames unique

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,11 +2,7 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   if Rails.env.test?
     storage :file
   else
@@ -51,5 +47,15 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 
 end
