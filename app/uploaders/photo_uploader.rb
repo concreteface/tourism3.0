71bf46cf86614d20456ce85325fbd0428d0aac6f
concreteface <concreteface@users.noreply.github.com>
2016-04-11 20:57:49 -0400

    fixed uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -41,15 +41,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
+  
+  unless Rails.env.test?
+    def filename
+      "#{secure_token}.#{file.extension}" if original_filename.present?
+    end
   end
 
   protected
