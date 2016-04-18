0331db5c30cbf30a86589ad99c1d3696cd02c7af
concreteface <concreteface@users.noreply.github.com>
2016-04-18 13:07:29 -0400

    changed test photo

--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe Attraction, type: :model do
 
     describe '#get_latitude' do
       it 'extracts latitude from a photo' do
-        expect(attraction.get_latitude('test.jpg')).to eq(42.7634)
+        expect(attraction.get_latitude('test.jpg')).to eq(35.098297111111115)
       end
     end
 
     describe '#get_longitude' do
       it 'extracts longitude from a photo' do
-        expect(attraction.get_longitude('test.jpg')).to eq(-71.21994722222222)
+        expect(attraction.get_longitude('test.jpg')).to eq(138.86338805555556)
       end
     end
 
     describe '#set_geolocation' do
       it 'sets geolocation on photo upload' do
         attraction.add_photo('test.jpg')
-        expect(attraction.latitude).to eq(42.7634)
-        expect(attraction.longitude).to eq(-71.21994722222222)
+        expect(attraction.latitude).to eq(35.098297111111115)
+        expect(attraction.longitude).to eq(138.86338805555556)
       end
     end
   end
