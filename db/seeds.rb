a7a8c47de19b15bee129965df086f836b0bb1b58
concreteface <concreteface@users.noreply.github.com>
2016-04-26 15:37:34 -0400

    added seed file, and a few small font tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user1 = User.create(username: 'jodama', email: 'john.jdm@gmail.com', admin: true, password: 'password')
+@user2 = User.create(username: 'rick', email: 'rick@grimes.com', password: 'password')
+
+@attraction1 = Attraction.new(name: 'Dog Fountain', description: 'I found it in downtown Numazu. Dogs never drink out of it. Only humans.', creator: @user2)
+@attraction1.add_photo('dog_fountain.jpg')
+@attraction1.save
+
+@attraction2 = Attraction.new(name: 'Odd Sculptures', description: 'Right outside Numazu City Hall.', creator: @user2)
+@attraction2.add_photo('weird_shapes.jpg')
+@attraction2.save
+
