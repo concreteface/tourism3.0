16725030f244b6295bd26eb4b8da45f4fc9051ab
concreteface <concreteface@users.noreply.github.com>
2016-04-29 07:53:42 -0400

    removed mailer

--- app/mailers/application_mailer.rb
+++
@@ -1,3 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "\"Sidestreeter HQ\" <no-reply@sidestreeter.herokuapp.com>"
-end
--- app/mailers/visit_mailer.rb
+++
@@ -1,9 +0,0 @@
-class VisitMailer < ApplicationMailer
-  def new_visit(visit)
-    @visit = visit
-    mail(
-      to: visit.visited_attraction.creator.email,
-      subject: "Someone visited #{visit.visited_attraction.name}!"
-    )
-  end
-end
