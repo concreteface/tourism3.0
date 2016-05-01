0460c5be8968c4cbd7d9382c1cc7aa493b0dd01a
concreteface <concreteface@users.noreply.github.com>
2016-04-30 21:25:15 -0400

    added about, fixed js testing, some style tweaks

--- spec/features/user_clicks_button_to_create_visit_spec.rb
+++ spec/features/user_clicks_button_to_create_visit_spec.rb
@@ -11,12 +11,24 @@ feature 'user can create a visit by clicking a button', js: true do
     login_as(@user)
     visit '/'
     click_button 'Been There'
-    page.reset!
     expect(page).not_to have_button('Been There')
   end
 
   scenario 'unauthenticated user visits attraction index page' do
     visit '/'
+
+    expect(page).not_to have_button('Been There')
+  end
+
+  scenario 'user deletes visit on profile page' do
+    @visit = FactoryGirl.create(:visit, visitor: @user, visited_attraction: @attraction)
+    login_as(@user)
+    visit user_path(@user)
+
+    click_link 'Your Visits'
+    expect(page).to have_content(@attraction.name)
+    click_button 'Nevermind'
+    expect(page).not_to have_content(@attraction.name)
 
     expect(page).not_to have_button('Been There')
   end
