c214f3b55a2e21e7e914403f21fd72175129d617
concreteface <concreteface@users.noreply.github.com>
2016-05-02 16:00:15 -0400

    changed default avatar location

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,7 +17,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
-    "avatar.png"
+    "#{Rails.root}/avatar.png"
   end
 
   # Process files as they are uploaded:
