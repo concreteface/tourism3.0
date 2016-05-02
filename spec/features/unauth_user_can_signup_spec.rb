05998f6c4e55558d311acf80c693b6d327d1a027
concreteface <concreteface@users.noreply.github.com>
2016-05-02 15:42:01 -0400

    added avatar to feature test

--- spec/features/unauth_user_can_signup_spec.rb
+++ spec/features/unauth_user_can_signup_spec.rb
@@ -10,12 +10,16 @@ feature "unauthenticated user can create account" do
     fill_in 'Username', with: 'yohoho'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
+    attach_file :user_avatar, "#{Rails.root}/avatar.png"
     click_button 'Sign up'
 
-    # expect(page).to have_content('yohoho')
     expect(page).to have_content('Signed up successfully.')
     expect(page).to have_content('Sign Out')
     expect(page).not_to have_content('Log in')
     expect(page).not_to have_content('Sign up')
+
+    click_link 'Profile'
+    expect(page).to have_content('yohoho')
+    expect(page).to have_css("img[src*='avatar.png']")
   end
 end
